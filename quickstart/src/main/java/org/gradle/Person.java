090918367e7a37713e2b7065c36246357f5d29d0
raultolive <yfert_0209@hotmail.com>
2019-07-02 19:39:01 -0400

    run analysis code and send email

--- quickstart/src/main/java/org/gradle/Person.java
+++ quickstart/src/main/java/org/gradle/Person.java
@@ -2,14 +2,28 @@ package org.gradle;
 
 import org.apache.commons.collections.list.GrowthList;
 
+/**
+ * Person class.
+ *
+ * @author Raul Choque
+ * @version 0.0.1
+ */
 public class Person {
     private final String name;
 
-    public Person(String name) {
+    /**
+     * Constructor method.
+     * @param name is name person
+     */
+    public Person(final String name) {
         this.name = name;
         new GrowthList();
     }
 
+    /**
+     * Return as a string the name.
+     * @return as a string a name
+     */
     public String getName() {
         return name;
     }
--- quickstart/src/test/java/org/gradle/PersonTest.java
+++ quickstart/src/test/java/org/gradle/PersonTest.java
@@ -1,9 +1,19 @@
 package org.gradle;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+/**
+ * PersonTest class.
+ *
+ * @author Raul Choque
+ * @version 0.0.1
+ */
 public class PersonTest {
+
+    /**
+     * Method to can construct a person with name.
+     */
     @Test
     public void canConstructAPersonWithAName() {
         Person person = new Person("Larry");
